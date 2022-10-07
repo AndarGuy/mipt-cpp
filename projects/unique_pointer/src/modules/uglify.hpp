92dd03384f36b2024b4995d11cde18f96cd668a9
Mikhail <mikhail@MacBook.lan>
2022-10-07 23:16:47 +0300

    add unique pointer

---
+++ projects/unique_pointer/src/modules/uglify.hpp
@@ -0,0 +1,6 @@
+#define UniquePtr UniquePointer
+#define Release release
+#define Reset reset
+#define Swap swap
+#define Get get
+#define MakeUnique make_unique
---
+++ projects/unique_pointer/src/modules/unique_pointer/unique_pointer.hpp
@@ -0,0 +1,84 @@
+#include <iostream>
+
+template <typename T>
+class UniquePointer {
+   public:
+    /// @brief Конструктор по умолчанию (создает нулевой указатель).
+    UniquePointer() = default;
+
+    /// @brief Конструктор от указателя (сохраняет указатель на объект).
+    /// @param ptr Указатель на объект
+    UniquePointer(T *ptr) : UniquePointer() {
+        this->pointer = ptr;
+    }
+
+    /// @brief Конструктор копирования и копирующее присваивание
+    /// должны быть явно запрещены.
+    UniquePointer(const UniquePointer &other) = delete;
+    /// @brief Конструктор копирования и копирующее присваивание
+    /// должны быть явно запрещены.
+    UniquePointer &operator=(const UniquePointer &other) = delete;
+
+    /// @brief Перемещающий конструктор и перемещающее присваивание
+    /// должны передавать владение объектом.
+    UniquePointer(const UniquePointer &&other) noexcept : UniquePointer() {
+        *this = (UniquePointer &&) other;
+    }
+
+    /// @brief Перемещающий конструктор и перемещающее присваивание
+    /// должны передавать владение объектом.
+    UniquePointer &operator=(UniquePointer &&other) noexcept {
+        this->pointer = std::move(other.pointer);
+        other.reset();
+        return *this;
+    }
+
+    /// @brief Метод `Release()`, который отлучает класс от владения
+    /// текущим ресурсом и возвращает указатель на него.
+    const T *release() {
+        T *temp = this->pointer;
+        this->reset();
+        return temp;
+    }
+
+    /// @brief Метод `Reset(T* ptr = nullptr)`, меняет указатель,
+    /// которым владеет объект (старый ресурс удаляется).
+    void reset(T *ptr = nullptr) {
+        this->pointer = ptr;
+    }
+
+    /// @brief Метод `Swap(UniquePtr&)`.
+    void swap(UniquePointer &other) {
+        std::swap(this->pointer, other.pointer);
+    }
+
+    /// @brief Метод `Get()`, возвращающий указатель на объект.
+    T *get() const {
+        return this->pointer;
+    }
+
+    /// @brief Оператор разыменовывания `operator*`.
+    T &operator*() const {
+        return *this->pointer;
+    }
+
+    /// @brief Оператор "стрелочка" `operator->`.
+    T *operator->() const { return this->pointer; }
+
+    /// @brief Оператор приведения к `bool` (`operator bool`).
+    operator bool() const {
+        return this->pointer != nullptr;
+    }
+
+   private:
+    T *pointer = nullptr;
+};
+
+/// @brief Реализуйте внешнюю шаблонную функцию `MakeUnique`,
+/// принимающую произвольное число параметров и возвращающую `UniquePtr`
+/// на объект созданный с помощью данных параметров конструктора.
+template <class T, typename... Args>
+UniquePointer<T> make_unique(Args... args) {
+    T *object = new T(args...);
+    return UniquePointer<T>(object);
+}
